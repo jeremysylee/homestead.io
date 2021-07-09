aedc91cd7679a104fdd6a53fbba0b910c8333fb8
Jeremy Lee <jeremysylee@gmail.com>
2021-07-08 15:28:38 -1000

    unsecure login

--- server/router.js
+++ server/router.js
@@ -1,10 +1,6 @@
-const path = require('path');
 const router = require('express').Router();
 const controller = require('./controllers/index');
 
-// router.get('/*', ((req, res) => {
-//   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-// }));
 router.get('/homes/:id/currentBid', controller.fetchCurrentBid);
 router.get('/homes/:id', controller.getHome);
 router.get('/homes/:id/bids', controller.getBids);
